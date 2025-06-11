22c0baa82fbd29b1e4a6023b6115b30834d5cbba
kothasaiprathyusha <kothasaiprathyusha896@gmail.com>
2025-06-11 17:25:14 +0530

    Add files via upload

---
+++ looplinked.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,data):
+        self.val=data
+        self.next=None
+def createlinkedlist(arr):
+    head=None    
+    for data in arr:
+        if(head==None):
+            head=Node(data)
+            temp=head
+        else:
+            temp.next=Node(data)
+            temp=temp.next 
+    temp.next=head.next       
+    print(detectloop(head))
+def detectloop(head):
+    
+    slow=head
+    fast=head 
+    while(fast and fast.next):
+        slow=slow.next
+        fast=fast.next.next
+        if(slow==fast):
+            return True  
+    return False      
+arr=list(map(int,input().split()))
+createlinkedlist(arr)               
+
+
